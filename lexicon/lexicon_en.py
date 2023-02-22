b014a7b750d38a2068537182e758ae8fac09f0c2
mazzdriver <oleg@massdriver95.ru>
2023-02-22 21:37:12 +0300

    english language 50%

---
+++ lexicon/lexicon_en.py
@@ -0,0 +1,24 @@
+LEXICON_EN: dict[str, str] = {
+    '/start': '<b>Hello!</b>\nShould you play the game '
+              '"Rock, paper, scissors"?\n\nIf you forgot '
+              'the rules, print /help for instructions.\n\n<b>Let\'s play!</b>',
+    '/help': "This is a very simple game. We should choose at"
+             "the same time one of these: rock, paper or scissors.\n\n"
+             "If we will choose the same both - it's draw."
+             "In other cases the rock breaks the skissors, "
+             "the scissors cut the paper and the paper rolls over"
+             "the rock. \n\n<b>Let's go!</b>"
+
+    'rock': 'Камень 🗿',
+    'paper': 'Бумага 📜',
+    'scissors': 'Ножницы ✂',
+    'yes_button': 'Давай!',
+    'no_button': 'Не хочу!',
+    'other_answer': 'Извини, этого я не понимаю',
+    'yes': 'Отлично! Делай свой выбор!',
+    'no': 'Жаль...\nЕсли захочешь сыграть, просто разверни '
+          'клавиатуру и нажми кнопку "Давай!"',
+    'bot_won': 'Я победил!\n\nСыграем еще?',
+    'user_won': 'Ты победил! Поздравляю!\n\nДавай сыграем еще?',
+    'nobody_won': 'Ничья!\n\nПродолжим?',
+    'bot_choice': 'Мой выбор'}
